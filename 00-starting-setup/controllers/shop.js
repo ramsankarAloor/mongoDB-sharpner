3d29a9d9545d6be9e3268f3507f455563b5fe48f
ramsankarAloor <ramsankar.aloor@gmail.com>
2023-12-14 11:50:27 +0530

    listing the orders

--- 00-starting-setup/controllers/shop.js
+++ 00-starting-setup/controllers/shop.js
@@ -92,8 +92,8 @@ exports.postOrder = (req, res, next) => {
 
       return order.save();
     })
-    .then((result)=>{
-      return req.user.clearCart()
+    .then((result) => {
+      return req.user.clearCart();
     })
     .then((result) => {
       res.redirect("/orders");
@@ -102,8 +102,7 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
+  Order.find({ "user.userId": req.user._id })
     .then((orders) => {
       res.render("shop/orders", {
         path: "/orders",
